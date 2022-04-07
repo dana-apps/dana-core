d720ed613a9bbfd80615ee0ec91c5b8e47c419ca
Chris Devereux <chrisdevereux@users.noreply.github.com>
2022-04-07 07:55:20 +0000

    workaround vite replacing process.env with '{}'

--- src/app/electron/config.ts
+++ src/app/electron/config.ts
@@ -3,7 +3,6 @@ import isDev from 'electron-is-dev';
 import { mkdir } from 'fs/promises';
 import path from 'path';
 import { z } from 'zod';
-import { getFileUrl } from '../util/platform';
 import { readJson, writeJson } from '../util/json-utils';
 
 const CONFIG_DIR = path.join(app.getPath('userData'), 'danacore');
@@ -14,14 +13,15 @@ const BUILD_ROOT = path.join(__dirname, '..', '..');
 /** Default frontend entrypoint (if not overriden through environment) */
 const DEFAULT_FRONTEND_SOURCE_URL = isDev
   ? 'http://localhost:3000/desktop.html'
-  : getFileUrl(path.join(BUILD_ROOT, 'build/renderer/desktop.html'));
+  : path.join(BUILD_ROOT, 'build/renderer/desktop.html');
 
 /** Enable developer tools */
-export const SHOW_DEVTOOLS = process.env.SHOW_DEVTOOLS || isDev ? true : false;
+export const SHOW_DEVTOOLS =
+  process.env['SHOW_DEVTOOLS'] || isDev ? true : false;
 
 /** Override frontend source URL */
 export const FRONTEND_SOURCE_URL =
-  process.env.FRONTEND_SOURCE_URL ?? DEFAULT_FRONTEND_SOURCE_URL;
+  process.env['FRONTEND_SOURCE_URL'] ?? DEFAULT_FRONTEND_SOURCE_URL;
 
 /**
  * Schema for the user's config file.
